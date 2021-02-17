a1a8e3ed9919c5bf97c4979b58e9231ec2314d3e
Andrea <andrea.mendez.sanz01@gmail.com>
2021-02-17 18:35:34 +0100

    Se crea la vista lista de listas. Se crea el gradiante y el fondo

---
+++ Android/app/src/main/java/com/example/nevera_andreaalejandra/Activities/ListaActivity.java
@@ -0,0 +1,16 @@
+package com.example.nevera_andreaalejandra.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nevera_andreaalejandra.R;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
