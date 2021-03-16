d109764f10a13ef8e16005e8935c3e40de81a949
Andrea <andrea.mendez.sanz01@gmail.com>
2021-03-16 11:35:30 +0100

    Se crean todas las vistas relacionadas con el Login

---
+++ Android/app/src/main/java/com/example/nevera_andreaalejandra/Fragments/Login/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.example.nevera_andreaalejandra.Fragments.Login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nevera_andreaalejandra.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ Android/app/src/main/java/com/example/nevera_andreaalejandra/Fragments/Login/LoginFragment.java
@@ -0,0 +1,33 @@
+package com.example.nevera_andreaalejandra.Fragments.Login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nevera_andreaalejandra.R;
+
+
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
